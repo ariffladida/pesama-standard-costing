fa90065ac50f79be93f523b9283c44de6264d178
ariffladida <ariffladida.works@gmail.com>
2026-09-05 20:19:55 +0800

    Add emergency web route to execute database migrations and seeders

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/run-emergency-migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    $migrateOutput = Artisan::output();
+
+    Artisan::call('db:seed', [
+        '--class' => 'CoaMouldingFjSeeder',
+        '--force' => true,
+    ]);
+    $seedOutput = Artisan::output();
+
+    return '<pre>--- MIGRATE OUTPUT ---' . PHP_EOL . $migrateOutput . PHP_EOL . PHP_EOL . '--- SEED OUTPUT ---' . PHP_EOL . $seedOutput . '</pre>';
+});
